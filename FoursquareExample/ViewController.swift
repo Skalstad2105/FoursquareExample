6df99ca62f9738d91a5b211814cdfaf178e01556
Marius Skalstad <Skalstad10@gmail.com>
2016-04-29 17:22:26 +0200

    Getting user location.

--- FoursquareExample/ViewController.swift
+++ FoursquareExample/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,MKMapViewDelegate{
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //Properties
+    let locationManager = CLLocationManager()
+    let distanceSpan:Double = 500
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.distanceFilter = 50 // Don't send location updates with a distance smaller than 50 meters between them
+        locationManager.startUpdatingLocation()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func viewDidAppear(animated: Bool)
+    {
+        checkLocationAuthorizationStatus()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nearbyLocation(sender: AnyObject) {
+        
+        mapView.setCenterCoordinate(mapView.userLocation.coordinate, animated: true)
 
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate
+{
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.04, longitudeDelta: 0.04))
+        self.mapView.setRegion(region, animated: true)
+        self.locationManager.stopUpdatingLocation()
+    }
 }
 
